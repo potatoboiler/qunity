cc93f70a1808ae56de62ffcce655301f20ff80b6
potatoboiler <61665896+potatoboiler@users.noreply.github.com>
2023-04-30 20:46:48 -0700

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,12 @@
 extern crate pest;
-#[macro_use]
 extern crate pest_derive;
 
 mod pest_parser;
-mod nom_parser;
-
-#[macro_use]
-extern crate nom;
 
 #[macro_use]
 extern crate lazy_static;
 
 pub mod parser;
-
-use std::collections::{hash_map, HashMap};
-
-use nom::IResult;
-
-type TokenName = String;
-enum TokenType {
-    Expression,
-    Type,
-    Program,
-    // TypeContext,
-}
-
-enum ValueType {
-    Void,
-    Unit, // ()
-    Sum,  // ?
-    Pair, // ?
-}
-
-
-
-struct Value {}
-
-struct CompilerContext {
-    scope_stack: Vec<Scope>,
-}
-
-struct Scope {
-    name: Option<String>,
-    bindings: HashMap<TokenName, TokenType>,
-}
-
-fn parse() -> IResult<String, Value> {
-    todo!()
-}
 
 fn main() {
     let file = std::fs::read_to_string("lol.q").expect("Noo file?");
